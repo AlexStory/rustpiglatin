f58d706c44b6a5a870f716ca2a67c44a7f3cafb7
Story <astory11@yahoo.com>
2017-06-16 07:56:02 -0500

    renamed handle to parse_word

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub fn run(words: String) -> String {
     let words_list = words.split_whitespace();
     let res: Vec<String> = words_list
-        .map(|x| handle(x.to_string()))
+        .map(|x| parse_word(x.to_string()))
         .collect();
     res.join(" ")
 }
@@ -10,7 +10,7 @@ fn is_lead_vowel(word: &String) -> bool {
     "aeiou".chars().any(|x| x.to_string() == first_letter(&word))
 }
 
-fn handle(word: String) -> String {
+fn parse_word(word: String) -> String {
     if is_lead_vowel(&word) {
         format!("{}-yay", &word)
     } else {

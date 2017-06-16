384f8bd495efaef4a040fad343e8e5fdb4042e7d
Alex Story <astory11@yahoo.com>
2017-06-15 21:00:10 -0500

    all the refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,27 @@
 pub fn run(words: String) -> String {
     let words_list = words.split_whitespace();
-    let res: Vec<String> = words_list.map(|x| handle(x.to_string())).collect();
+    let res: Vec<String> = words_list
+        .map(|x| handle(x.to_string()))
+        .collect();
     res.join(" ")
 }
 
 fn is_lead_vowel(word: &String) -> bool {
     let vowels = String::from("aeiou");
-    let first: String = word.chars().take(1).collect();
+    let first = first_letter(&word);
     vowels.chars().any(|x| x.to_string() == first)
 }
 
 fn handle(word: String) -> String {
-    if let true = is_lead_vowel(&word) {
-        let temp = String::new();
-        let mut temp = temp + &word;
-        temp.push_str("-yay");
-        temp
+    if is_lead_vowel(&word) {
+        format!("{}-yay", &word)
     } else {
-        let mut temp = String::new();
-        let first: String = word.chars().take(1).collect();
-        temp.push_str(&word[1..]);
-        let mut temp = temp + &first;
-        temp.push_str("ay");
-        temp
+        let first = first_letter(&word);
+        format!("{}{}ay", &word[1..], first)
     }
+}
+
+fn first_letter(word: &String) -> String {
+    word.chars().take(1).collect()
 }
 

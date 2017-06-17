9943ed0b0f07b1f01b2c159d1b72b93e71fbeaf4
Alex Story <astory11@yahoo.com>
2017-06-17 11:01:13 -0500

    dogfooding my string crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+extern crate yarns;
+
 pub fn run(words: String) -> String {
     let words_list = words.split_whitespace();
     let res: Vec<String> = words_list
@@ -7,17 +9,15 @@ pub fn run(words: String) -> String {
 }
 
 fn is_lead_vowel(word: &String) -> bool {
-    "aeiou".chars().any(|x| x.to_string() == first_letter(&word))
+    let vowels = String::from("aeiou");
+    let head = yarns::head(&word);
+    yarns::contains(&vowels, &head)
 }
 
 fn parse_word(word: String) -> String {
     match is_lead_vowel(&word) {
         true => format!("{}-yay", &word),
-        false => format!("{}{}ay", &word[1..], first_letter(&word))
+        false => format!("{}{}ay", &word[1..], yarns::head(&word))
     }
-}
-
-fn first_letter(word: &String) -> String {
-    word.chars().take(1).collect()
 }
 
